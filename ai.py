2d234b716571332bf34574074e311ca3791ede79
George Codat Tudoran <office@solcreation.ro>
2020-01-19 12:07:00 +0200

    create the architecture of the neural network

--- ai.py
+++ ai.py
@@ -28,11 +28,11 @@ class CNN(nn.Module):
     
     def __init__(self, number_actions):
         super(CNN, self).__init__()
-        self.convolution1
-        self.convolution2
-        self.convolution3
-        self.fc1
-        self.fc2
+        self.convolution1 = nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 5)
+        self.convolution2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3)
+        self.convolution3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 2)
+        self.fc1 = nn.Linear(in_features = number_neurons, out_features = 40)
+        self.fc2 = nn.Linear(in_features = 40, out_features = number_actions)
 
 # Making the body
 
